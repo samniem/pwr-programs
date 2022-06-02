f1cc397c21634835884f1db3b4fdd6a6131790a5
samniem <sami.nieminen.hw@gmail.com>
2022-06-02 13:10:01 +0900

    Get Exercise Names on Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import React from "react"
-import res from 'express/lib/response';
 
 function App() {
 
-  const [data, setData] = React.useState(null)
-
+  //const [data, setData] = React.useState(null)
+  const [exercises, setExercises] = React.useState(null)
+  /*
   React.useEffect(() => {
     fetch("/api").then((res) => res.json()).then((data) => setData(data.message))
+  })*/
+
+  React.useEffect(() => {
+    fetch("/api/exercises")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("DATA", data)
+      setExercises(data)
+    })
   })
 
   return <div>
-
+    <p><ul>{!exercises ? "Loading exercises..." : exercises.map(e => <li>{e.name}</li>)}</ul></p>
   </div>
 }
 
--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 // server/index.js
 
-const express = require("express");
+const express = require("express")
+const fs = require('fs')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-const app = express();
+const app = express()
 
 app.get("/api", (_, res) => {
     res.json({ message: "Hello! Request received!"})
 })
 
+app.get("/api/exercises", (_, res) => {
+    const exercises = JSON.parse(fs.readFileSync("server/exercises.json"))
+    console.log("sending exercises", exercises)
+    res.json(exercises)
+})
+
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`)
 })
